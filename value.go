e359d39129b99ed7824dd3fa1ce3ae8b6faa395e
Roy Brabson <rbrabson@gmail.com>
2025-06-08 22:43:39 -0400

    tests

--- value.go
+++ value.go
@@ -1,6 +1,6 @@
 package dice
 
-// Value is the value returned by a dice roll or difficulty class.
+// Value is the value returned by a die roll or difficulty class.
 type Value interface {
 	Value() int           // Returns the value of the variable
 	Check(v Value) bool   // Determines if the roll has a value equal-to-or-greater than the value passed in for the check.
